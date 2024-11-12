ca1d1f21b316c6115dace11bd2432faf453dad4e
Ivo Araujo <ivo.souza-23@outlook.com>
2024-11-12 10:59:37 -0300

    Criado dbjson, interface e adicionado o ng bootstrap

---
+++ src/app/Agenda.ts
@@ -0,0 +1,8 @@
+export interface Agenda {
+    id: number,
+    titulo: string,
+    descricao: string,
+    data: string,
+    hora: string,
+    local: string
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { FormCompromissosComponent } from './form-compromissos/form-compromissos
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [
     provideClientHydration()
