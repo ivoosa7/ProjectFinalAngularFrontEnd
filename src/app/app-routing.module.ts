efa37f77a9b2e010806dc11ebacc0d64a87c6c98
Ivo Araujo <ivo.souza-23@outlook.com>
2024-11-12 11:17:17 -0300

    FrontEnd feito com navbar e footer, e rotas criadas no app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { VerCompromissosComponent } from './ver-compromissos/ver-compromissos.component';
+import { FormCompromissosComponent } from './form-compromissos/form-compromissos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'Home', component: HomeComponent},
+  {path: 'compromissos', component: VerCompromissosComponent},
+  {path: 'formcompromissos', component:FormCompromissosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
